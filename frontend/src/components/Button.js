054f8527b31bd1d4dea5a204d25b7995126db2d1
Chanel <>
2021-09-16 13:08:49 +0800

    cleanup button

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -2,17 +2,9 @@ import React from 'react';
 import '../App.css';
 
 export default function Button({ label, handleClick, variant }) {
-    if (label !== "Reset") {
-        return (
-            <button onClick={handleClick} variant={variant} className="btn" >
-                {label}
-            </button>
-        );
-    } else if (label === "Reset") {
-        return (
-            <button onClick={handleClick} variant={variant} className="btn2">
-                {label}
-            </button>
-        );
-    }
+    return (
+        <button onClick={handleClick} variant={variant} className={label === "Reset" ? "btn2" : "btn"} >
+            {label}
+        </button>
+    );
 }
